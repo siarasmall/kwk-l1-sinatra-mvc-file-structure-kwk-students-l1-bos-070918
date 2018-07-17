924945e89cb2be5f514e84d06838b69b69a51374
siarasmall <siarasmall19@gmail.com>
2018-07-17 18:00:20 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,16 +1,16 @@
 class Dog
-  attr_accessor :name, :age, :breed
+  attr_accessor :name, :breed, :age
   @@dog_array = []
   
-  def initialize(name,age,breed)
+  def initialize(name,breed,age)
     @name = name 
-    @breed = breed
     @age = age
+    @breed = breed
     @@dog_array << self
   end 
   
   def self.all 
-    @dog_array
+    @@dog_array
   end 
   
 end 
